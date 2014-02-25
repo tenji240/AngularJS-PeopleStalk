bcc301f7a3e21ab6301aff56860f718e08364730
Tenji Tembo <us74092@umbc.edu>
2014-02-24 23:49:44 -0500

    Dynamic Table Render
    Almost Freaking done!!!!
    Just need to add and remove users + handle authentication

--- database.js
+++ database.js
@@ -27,12 +27,31 @@ function addUser(fullname, email, phone) {
 }
 
 function loadTable() {
-    for(i = 0; i < user.length; i++){
-        document.write('<tr>');
-        document.write('<td>' + user[i].fullname + '</td>');
-        document.write('<td>' + users[i].email + '</td>');
-        document.write('<td>' + users[i].phone + '</td>');
-        document.write('</tr>');
+    for(i = 0; i < users.length; i++){
+        
+        //if(!document.getElementsByTagName) return;
+        
+        tabBody = document.getElementsByTagName('tbody').item(0);
+        
+        row = document.createElement('tr');
+        
+        cell_1 = document.createElement('td');
+        cell_2 = document.createElement('td');
+        cell_3 = document.createElement('td');
+            
+        textNode_1 = document.createTextNode(users[i].fullname);
+        textNode_2 = document.createTextNode(users[i].email);
+        textNode_3 = document.createTextNode(users[i].phone);
+        
+        cell_1.appendChild(textNode_1);
+        cell_2.appendChild(textNode_2);
+        cell_3.appendChild(textNode_3);
+        
+        row.appendChild(cell_1);
+        row.appendChild(cell_2);
+        row.appendChild(cell_3);
+        
+        tabBody.appendChild(row);
     }
 }
 
