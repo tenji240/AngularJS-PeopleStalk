e23f94e0fa8fe429540c76dce57bdc501bf68771
Tenji Tembo <us74092@umbc.edu>
2014-02-24 23:57:20 -0500

    Added some comments
    comments to make life easier. Also making a pull request so HANG ON!!

--- database.js
+++ database.js
@@ -1,10 +1,14 @@
 /*
 * This is the database JS Handler for the top level 
 * of the application. 
+* Author - Tenji Tembo
 */
 
+//Global Stuff for rendering the TABLE
 var users = [];
+var temp;
 
+//magical employee constructor - hopefully port to JSON Object
 function employee(fullname, email, phone, picture) {
     this.fullname = fullname;
     this.email = email;
@@ -12,6 +16,8 @@ function employee(fullname, email, phone, picture) {
     this.picture = picture;
 }
 
+//returns the employee object...
+//dunno why....
 function getEmployee(fullname) {
     var i;
     for (i = 0; i < users.length; i++){
@@ -21,11 +27,16 @@ function getEmployee(fullname) {
     }
 }
 
+//add in a new user 
+//untested....
 function addUser(fullname, email, phone) {
-    var newUser = employee(fullname ,email ,phone ,null);
+    temp = employee(fullname ,email ,phone ,null);
     users.push(newUser);
 }
 
+//creates the intial set of users
+//pulls from the initial table of users
+//update the global object array to add and display new users....
 function loadTable() {
     for(i = 0; i < users.length; i++){
         
@@ -33,32 +44,40 @@ function loadTable() {
         
         tabBody = document.getElementsByTagName('tbody').item(0);
         
+        //a row elements
         row = document.createElement('tr');
         
+        //three column elements
         cell_1 = document.createElement('td');
         cell_2 = document.createElement('td');
         cell_3 = document.createElement('td');
             
+        //added corresponding elements
         textNode_1 = document.createTextNode(users[i].fullname);
         textNode_2 = document.createTextNode(users[i].email);
         textNode_3 = document.createTextNode(users[i].phone);
         
+        //attached the information to the cells
         cell_1.appendChild(textNode_1);
         cell_2.appendChild(textNode_2);
         cell_3.appendChild(textNode_3);
         
+        //attached the cells to the row
         row.appendChild(cell_1);
         row.appendChild(cell_2);
         row.appendChild(cell_3);
         
+        //attach the row to the body
         tabBody.appendChild(row);
     }
 }
 
+//three global objects just to make life easier - exists in the DB
 var a = new employee('Tenji Tembo', 'tenji@go.com', 2024102211);
 var b = new employee('James Patt', 'james@ball.co', 2021114421);
 var c = new employee('Jim Morty', 'jim_morty@aol.com', 2228881166);
 
+//without this, no initial users.
 function sample() {
     users.push(a);
     users.push(b);
