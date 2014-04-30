16d144bf2fc06c358135db10dca0c737c21d5614
Tenji Tembo <us74092@umbc.edu>
2014-04-30 15:42:07 -0400

    Finishing up Project
    All Functionality Should be present, now going through testing and
    debugging.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var clear = false;
 var update = false;
 var array = [
-        {id : 0, name : 'Alpha', email : 'alpha@gmail.com', phone : '111-222-3333', url : 'http://tinyurl.com/k9oey4q', password : 'paper56'},
-        {id : 1, name : 'Beta', email : 'beta@gmail.com', phone : '111-222-3345', url : 'http://tinyurl.com/jvwsjcl', password : 'bell541'},
-        {id : 2, name : 'Charlie', email : 'charlie@gmail.com', phone : '111-223-4456', url : 'http://tinyurl.com/layxu5v', password : 'calling131'},
-        {id : 3, name : 'Delta', email : 'delta@gmail.com', phone : '111-445-2213', url : 'http://tinyurl.com/kfmjv2l', password : 'wankel123'}
+        {id : 0, name : 'Alpha', email : 'alpha@gmail.com', phone : '111-222-3333', url : 'http://tinyurl.com/k9oey4q', password : 'paper56', updatePassword:true, securityQuestion:'What is my favorite Color?', securityAnswer:'Sky Blue'},
+        {id : 1, name : 'Beta', email : 'beta@gmail.com', phone : '111-222-3345', url : 'http://tinyurl.com/jvwsjcl', password : 'bell541', updatePassword:true, securityQuestion:'What is my pet\'s name?', securityAnswer:'Benji'},
+        {id : 2, name : 'Charlie', email : 'charlie@gmail.com', phone : '111-223-4456', url : 'http://tinyurl.com/layxu5v', password : 'calling131', updatePassword:false, securityQuestion:'What is my favorite Car brand?', securityAnswer:'BMW'},
+        {id : 3, name : 'Delta', email : 'delta@gmail.com', phone : '111-445-2213', url : 'http://tinyurl.com/kfmjv2l', password : 'wankel123', updatePassword:false, securityQuestion:'Where was my last Job?', securityAnswer:'Baltimore'}
     ];
 
 var peopleApp = angular.module('peopleApp', ['ngRoute']);
@@ -74,47 +74,87 @@ peopleApp.controller('loginController', [ '$scope', '$location', function ($scop
         
         var found = false;
         
-        for(var data = 0; data < array.length; data++){
-            if(array[data].email == $scope.user.email && array[data].password == $scope.user.password){
-                window.alert('Welome - Click to Redirect'); 
-                found = true;
-                break;
+        if(testEmail($scope.user.email)){
+            for(var data = 0; data < array.length; data++){
+                if(array[data].email == $scope.user.email){
+                    
+                    if(array[data].password == $scope.user.password){
+                        window.alert('Welome Back'); 
+                        found = true;
+                        
+                        if(array[data].updatePassword == true)
+                            window.alert('Please Update your Password');
+                        
+                        break;
+                    }else{
+                        var answer = window.prompt(array[data].securityQuestion);   
+                        if(answer == array[data].securityAnswer){
+                            window.alert('Welome Back'); 
+                            found = true;
+                            break;
+                        }
+                    }
+                    
+                }
             }
-            else
-                window.alert('Invalid Email and/or Password');
         }
         
+        //Solved Redirection
         if(found){
-            window.location('/#home');
+            window.location = 'http://127.0.0.1:8888/#/home';
+            window.location.reload();
+        }else{
+            window.alert('Invalid Username or Password');
         }
     }
 }]);
 
+function testEmail(elem){
+    var patt = new RegExp(/^[a-zA-Z0-9_]*@[a-zA-Z0-9_]*\.[a-zA-Z0-9_]*$/);
+    return patt.test(elem);
+}
+
+function testPhone(elem){
+    var patt = new RegExp(/^\+?\d?[- ]?\d{3}[- ]?\d{3}[- ]?\d{4}$/);
+    return patt.test(elem);
+}
+
 //Signup Controller
-peopleApp.controller('signupController', function ($scope) {
+peopleApp.controller('signupController', function ($scope, $window) {
     $scope.master = array;
     
     $scope.signUp = function(){
         var found = false;
         
-        for(var data = 0; data < array.length; data++){
-            if(array[data].name == $scope.user.name){
-                found = true;
-                break;
+        if(testEmail($scope.user.email) && testPhone($scope.user.phone)){
+            for(var data = 0; data < array.length; data++){
+                if(array[data].name == $scope.user.name){
+                    found = true;
+                    break;
+                }
             }
-        }
-        
-        if(!found){
-            array.push({
-                id : array.length,
-                name : $scope.user.name,
-                email : $scope.user.email,
-                phone : $scope.user.phone,
-                password : $scope.user.password
-            });
-        
-            window.alert('Welcome, ' + $scope.user.name +  ' to PeopleStalk.');
-            window.location('#home');
+
+            if(!found){
+                var dataObject = {
+                    id : array.length,
+                    name : $scope.user.name,
+                    email : $scope.user.email,
+                    phone : $scope.user.phone,
+                    url : $scope.user.url,
+                    password : $scope.user.password,
+                    updatePassword: false,
+                    securityQuestion: $scope.user.securityQuestion,
+                    securityAnswer: $scope.user.securityAnswer,
+                    option: "insert"
+                };
+                
+                array.push(dataObject);
+                addUser_PostR(dataObject);
+
+                window.alert('Welcome, ' + $scope.user.name +  ' to PeopleStalk.');
+                window.location = 'http://127.0.0.1:8888/#/home';
+                window.location.reload();
+            }
         }
     }
 });
@@ -133,11 +173,14 @@ peopleApp.controller('manageController', function ($scope, $window) {
             phone : $scope.user.phone,
             url : $scope.user.url,
             password : $scope.user.password,
+            updatePassword: false,
+            securityQuestion: $scope.user.securityQuestion,
+            securityAnswer: $scope.user.securityAnswer,
             option: "insert"
         };
-
+        
+        //adding to backup cache as well as data array
         array.push(dataObject);
-        $window.alert('Successfully Added ' + $scope.user.name +  ' to PeopleStalk.');
         addUser_PostR(dataObject);
     }
     
@@ -169,6 +212,9 @@ peopleApp.controller('manageController', function ($scope, $window) {
             phone : $scope.user.phone,
             url : $scope.user.url,
             password : $scope.user.password,
+            updatePassword: false,
+            securityQuestion: $scope.user.securityQuestion,
+            securityAnswer: $scope.user.securityAnswer,
             option: "insert"
         };
         
@@ -179,6 +225,8 @@ peopleApp.controller('manageController', function ($scope, $window) {
                 array[data].phone = $scope.user.phone;
                 array[data].url = $scope.user.url;
                 array[data].password = $scope.user.password;
+                array[data].securityQuestion: $scope.user.securityQuestion;
+                array[data].securityAnswer = $scope.user.securityAnswer;
                 found = true;
                 break;
             }
@@ -204,6 +252,15 @@ peopleApp.controller('friendsController', function($scope) {
     $scope.message = 'Anyone and Everyone you know is displayed here!';
     
     $scope.master = array;
+        
+    $scope.getTable = function(){
+        var tabl = getTable_PostR();
+        window.alert(tabl);
+        array.concat(tabl);
+        
+        $scope.extra = tabl;
+    }
+    
 });
 
 peopleApp.controller('profileController', function($scope){
@@ -240,10 +297,10 @@ function addUser_PostR(dataObject){
         data: JSON.stringify(dataObject),
         datatype: "json",
         contentType: 'application/json; charset=utf-8',
-        success: function(result) { alert("Completed Post Request"); },
+        success: function(result) { window.alert(result); },
         error: function(xmlhdrq, ajaxOptions, thrownError) {
-            alert(xmlhdrq.status);
-            alert(thrownError);
+            window.alert(xmlhdrq.status);
+            window.alert(thrownError);
         }
     });
 } 
@@ -256,10 +313,39 @@ function removeUser_PostR(dataObject){
         data: JSON.stringify(dataObject),
         datatype: "json",
         contentType: 'application/json; charset=utf-8',
-        success: function(result) { alert("Removed " + dataObject.name + " From Database."); },
+        success: function(result) { window.alert(result); },
+        error: function(xmlhdrq, ajaxOptions, thrownError) {
+            window.alert(xmlhdrq.status);
+            window.alert(thrownError);
+        }
+    });
+}
+
+function getTable_PostR(){
+    
+    var dataobject;
+    var elem;
+    //POST REQUEST - Server Side JS - Excutes Asynchronously which is a pain...
+    
+    dataobject = {option:'show'};
+    $.ajax({
+        async:true,
+        type: "POST",
+        url: "http://127.0.0.1:8888/",
+        data: JSON.stringify(dataobject),
+        datatype: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(result) { 
+            console.log(result);
+            elem = result; 
+            console.log(elem);
+        },
         error: function(xmlhdrq, ajaxOptions, thrownError) {
             alert(xmlhdrq.status);
             alert(thrownError);
         }
     });
+    
+    console.log(elem);
+    return elem;
 }
